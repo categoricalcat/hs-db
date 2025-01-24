ebcf27744e2076580524698ab795e1d343201d60
categoricalcat <catufuzgu@gmail.com>
2025-01-24 17:33:07 -0300

    Add environment variable setup from .env file in Main module
    - Introduced `setEnvs` function to read and set environment variables from the .env file.
    - Integrated environment variable loading into the main execution flow.
    - Removed redundant .env parsing logic from the main function for cleaner code.

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,31 @@
 
 module Main where
 
-import Control.Applicative (Alternative (..))
 import DB.Helpers (describeConnection)
 import DB.Main (getConn, query, withQueryHandler)
-import Data.Char (isUpperCase)
 import Data.Map qualified as Map
 import Database.HDBC (IConnection (..), SqlValue (SqlInteger))
 import MyLib (safeReadFile, withTaskLog)
 import Parser.Main
+  ( Parser (runParser),
+    ParserLog (ParserLog),
+    envKeyValues,
+  )
+import System.Environment (setEnv)
+
+setEnvs :: IO ()
+setEnvs =
+  safeReadFile ".env"
+    >>= ( \ma -> case ma of
+            Right s -> return $ runParser envKeyValues s []
+            Left _ -> return $ (Nothing, [], [ParserLog "no_env_file"])
+        )
+    >>= ( \ma -> case ma of
+            (Just envs, _, _) -> do
+              mapM_ (\(k, v) -> setEnv k v) envs
+              putStrLn "Environment variables set successfully."
+            _ -> putStrLn "No environment variables to set."
+        )
 
 run :: (IConnection conn) => conn -> IO ()
 run conn = do
@@ -29,6 +46,9 @@ run conn = do
     query conn "SELECT ($1::integer) + ($2::integer)" [SqlInteger 2, SqlInteger 2]
       >>= print
 
+  withTaskLog "setting up loading envs from .env" $
+    setEnvs
+
   return ()
 
 main :: IO ()
@@ -39,11 +59,5 @@ main = withTaskLog "maine" $ do
     >>= putStrLn
 
   Main.run conn
-  safeReadFile ".env"
-    >>= ( \ma -> case ma of
-            Right s -> return $ runParser envKeyValues s []
-            Left _ -> return $ (Nothing, [], [ParserLog "no_env_file"])
-        )
-    >>= print
 
   disconnect conn
